d794e34cd9291f439875f9ece62e4f15b0f705ef
vite <nuget@list.ru>
2024-04-23 07:31:20 +0300

    cleanup test/locations.js

--- test/locations.js
+++ test/locations.js
@@ -1,31 +1,23 @@
-import assert from 'assert';
-import Locations from '../src/locations';
-import * as core from '../src/utils/core';
+import assert from "assert"
+import Locations from "../src/locations"
+import * as core from "../src/utils/core"
 
-describe('Locations', function() {
-
-	describe('#parse', function() {
-		var chapter = require('./fixtures/locations.xhtml').default;
-
-		it('parse locations from a document', function() {			
-			var doc = core.parse(chapter, "application/xhtml+xml");
-			var contents = doc.documentElement;
-			var locations = new Locations();
-			var result = locations.parse(contents, "/6/4[chap01ref]", 100);
-			assert.equal(result.length, 15);
-
-		});
-
-		it('parse locations from xmldom', function() {
-			var doc = core.parse(chapter, "application/xhtml+xml", true);
-			var contents = doc.documentElement;
-
-			var locations = new Locations();
-			var result = locations.parse(contents, "/6/4[chap01ref]", 100);
-			assert.equal(result.length, 15);
-
-		});
-
-	});
-
-});
+describe("Locations", () => {
+	describe("#parse", () => {
+		const chapter = require('./fixtures/locations.xhtml').default
+		it("parse locations from a document", () => {
+			const doc = core.parse(chapter, "application/xhtml+xml")
+			const contents = doc.documentElement
+			const locations = new Locations()
+			const result = locations.parse(contents, "/6/4[chap01ref]", 100)
+			assert.equal(result.length, 15)
+		})
+		it("parse locations from xmldom", () => {
+			const doc = core.parse(chapter, "application/xhtml+xml", true)
+			const contents = doc.documentElement
+			const locations = new Locations()
+			const result = locations.parse(contents, "/6/4[chap01ref]", 100)
+			assert.equal(result.length, 15)
+		})
+	})
+})
