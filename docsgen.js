6f59e2e34c714f4bd923441602995f140e7b552c
vite <nuget@list.ru>
2024-04-25 22:58:44 +0300

    added docsgen.js script

---
+++ docsgen.js
@@ -0,0 +1,74 @@
+const fs = require("node:fs")
+const path = require("path")
+const jsdoc2md = require("jsdoc-to-markdown")
+const output = (dir, filename, data) => {
+    try {
+        fs.mkdirSync(dir, { recursive: true }, err => {
+            if (err) throw err
+        })
+        const ws = fs.createWriteStream(filename)
+        ws.write(data)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+const docs = [
+    "annotations",
+    "archive",
+    "container",
+    "contents",
+    "displayoptions",
+    "epub",
+    "epubcfi",
+    "layout",
+    "locations",
+    "managers/continuous/index",
+    "managers/default/index",
+    "managers/helpers/snap",
+    "managers/helpers/stage",
+    "managers/views/iframe",
+    "mapping",
+    "navigation",
+    "packaging",
+    "pagelist",
+    "rendition",
+    "resources",
+    "section",
+    "spine",
+    "store",
+    "themes",
+    "utils/constants",
+    "utils/core",
+    "utils/hook",
+    "utils/mime",
+    "utils/path",
+    "utils/replacements",
+    "utils/request",
+    "utils/scrolltype",
+    "utils/queue",
+    "utils/url"
+]
+
+for (const doc of docs) {
+    const time = Date.now()
+    const item = doc.split('/')
+    let opath = path.resolve(__dirname, "docs")
+    let ifile = "src"
+    let ofile = opath
+    for (let i = 0, len = item.length; i < len; ++i) {
+        if (i === (len - 1)) {
+            ifile = path.join(ifile, item[i]) + ".js"
+            ofile = path.join(ofile, item[i]) + ".md"
+        } else {
+            opath = path.join(opath, item[i])
+            ifile = path.join(ifile, item[i])
+            ofile = path.join(ofile, item[i])
+        }
+    }
+    jsdoc2md.render({ files: ifile }).then(data => {
+        output(opath, ofile, data)
+        const t = (Date.now() - time).toString().padStart(3)
+        console.log("output [time:%s ms, path:%s]", t, ofile)
+    })
+}
