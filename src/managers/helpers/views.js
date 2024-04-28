0592e80738d15d6c338d44993800aedcbf0f2539
vite <nuget@list.ru>
2024-04-28 06:18:25 +0300

    refactoring src/managers/helpers/views.js

--- src/managers/helpers/views.js
+++ src/managers/helpers/views.js
@@ -1,5 +1,13 @@
+/**
+ * Views
+ */
 class Views {
+	/**
+	 * Constructor
+	 * @param {Element} container 
+	 */
 	constructor(container) {
+
 		this.container = container;
 		this._views = [];
 		this.length = 0;
@@ -7,41 +15,49 @@ class Views {
 	}
 
 	all() {
+
 		return this._views;
 	}
 
 	first() {
+
 		return this._views[0];
 	}
 
 	last() {
-		return this._views[this._views.length-1];
+
+		return this._views[this._views.length - 1];
 	}
 
 	indexOf(view) {
+
 		return this._views.indexOf(view);
 	}
 
 	slice() {
+
 		return this._views.slice.apply(this._views, arguments);
 	}
 
 	get(i) {
+
 		return this._views[i];
 	}
 
-	append(view){
+	append(view) {
+
 		this._views.push(view);
-		if(this.container){
+		if (this.container) {
 			this.container.appendChild(view.element);
 		}
 		this.length++;
 		return view;
 	}
 
-	prepend(view){
+	prepend(view) {
+
 		this._views.unshift(view);
-		if(this.container){
+		if (this.container) {
 			this.container.insertBefore(view.element, this.container.firstChild);
 		}
 		this.length++;
@@ -49,10 +65,11 @@ class Views {
 	}
 
 	insert(view, index) {
+
 		this._views.splice(index, 0, view);
 
-		if(this.container){
-			if(index < this.container.children.length){
+		if (this.container) {
+			if (index < this.container.children.length) {
 				this.container.insertBefore(view.element, this.container.children[index]);
 			} else {
 				this.container.appendChild(view.element);
@@ -64,25 +81,23 @@ class Views {
 	}
 
 	remove(view) {
-		var index = this._views.indexOf(view);
 
-		if(index > -1) {
+		const index = this._views.indexOf(view);
+		if (index > -1) {
 			this._views.splice(index, 1);
 		}
-
-
 		this.destroy(view);
-
 		this.length--;
 	}
 
 	destroy(view) {
-		if(view.displayed){
+
+		if (view.displayed) {
 			view.destroy();
 		}
-		
-		if(this.container){
-			 this.container.removeChild(view.element);
+
+		if (this.container) {
+			this.container.removeChild(view.element);
 		}
 		view = null;
 	}
@@ -90,18 +105,17 @@ class Views {
 	// Iterators
 
 	forEach() {
+
 		return this._views.forEach.apply(this._views, arguments);
 	}
 
-	clear(){
+	clear() {
+
 		// Remove all views
-		var view;
-		var len = this.length;
+		if (this.length === 0) return;
 
-		if(!this.length) return;
-
-		for (var i = 0; i < len; i++) {
-			view = this._views[i];
+		for (let i = 0; i < this.length; i++) {
+			const view = this._views[i];
 			this.destroy(view);
 		}
 
@@ -109,54 +123,45 @@ class Views {
 		this.length = 0;
 	}
 
-	find(section){
+	find(section) {
 
-		var view;
-		var len = this.length;
-
-		for (var i = 0; i < len; i++) {
-			view = this._views[i];
-			if(view.displayed && view.section.index == section.index) {
+		for (let i = 0; i < this.length; i++) {
+			const view = this._views[i];
+			if (view.displayed && 
+				view.section.index == section.index) {
 				return view;
 			}
 		}
-
 	}
 
-	displayed(){
-		var displayed = [];
-		var view;
-		var len = this.length;
+	displayed() {
 
-		for (var i = 0; i < len; i++) {
-			view = this._views[i];
-			if(view.displayed){
+		const displayed = [];
+		for (let i = 0; i < this.length; i++) {
+			const view = this._views[i];
+			if (view.displayed) {
 				displayed.push(view);
 			}
 		}
 		return displayed;
 	}
 
-	show(){
-		var view;
-		var len = this.length;
+	show() {
 
-		for (var i = 0; i < len; i++) {
-			view = this._views[i];
-			if(view.displayed){
+		for (let i = 0; i < this.length; i++) {
+			const view = this._views[i];
+			if (view.displayed) {
 				view.show();
 			}
 		}
 		this.hidden = false;
 	}
 
-	hide(){
-		var view;
-		var len = this.length;
+	hide() {
 
-		for (var i = 0; i < len; i++) {
-			view = this._views[i];
-			if(view.displayed){
+		for (let i = 0; i < this.length; i++) {
+			const view = this._views[i];
+			if (view.displayed) {
 				view.hide();
 			}
 		}
