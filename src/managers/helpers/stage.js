5733da19f6f3d14d2f519619c14d673fc8503a44
vite <nuget@list.ru>
2024-05-01 04:11:14 +0300

    refactoring src/managers/helpers/stage.js

--- src/managers/helpers/stage.js
+++ src/managers/helpers/stage.js
@@ -1,26 +1,42 @@
-import {uuid, isNumber, isElement, windowBounds, extend} from "../../utils/core";
+import { uuid, isNumber, isElement, windowBounds, extend } from "../../utils/core";
 import throttle from 'lodash/throttle'
 
 /**
  * Stage
- * @param {object} options
- * @param {string} options.axis
- * @param {string} options.direction
- * @param {boolean} options.fullsize
- * @param {string|number} options.width
- * @param {string|number} options.height
  */
 class Stage {
+	/**
+	 * Constructor
+	 * @param {object} options
+	 * @param {string} options.axis
+	 * @param {string} options.direction
+	 * @param {boolean} options.fullsize
+	 * @param {string|number} options.width
+	 * @param {string|number} options.height
+	 */
 	constructor(options) {
+		/**
+		 * @member {object} settings
+		 * @memberof Stage
+		 * @readonly
+		 */
 		this.settings = options || {};
+		/**
+		 * @member {string} id
+		 * @memberof Stage
+		 * @readonly
+		 */
 		this.id = "epubjs-container-" + uuid();
-
+		/**
+		 * @member {Element} container
+		 * @memberof Stage
+		 * @readonly
+		 */
 		this.container = this.create(this.settings);
 
-		if(this.settings.hidden) {
+		if (this.settings.hidden) {
 			this.wrapper = this.wrap(this.container);
 		}
-
 	}
 
 	/**
@@ -29,48 +45,46 @@ class Stage {
 	 * @param {object} options 
 	 * @returns {Element} container
 	 */
-	create(options){
-		let height  = options.height;// !== false ? options.height : "100%";
-		let width   = options.width;// !== false ? options.width : "100%";
-		let overflow  = options.overflow || false;
+	create(options) {
+
+		let height = options.height; // !== false ? options.height : "100%";
+		let width = options.width; // !== false ? options.width : "100%";
+		let overflow = options.overflow || false;
 		let axis = options.axis || "vertical";
 		let direction = options.direction;
 
 		extend(this.settings, options);
 
-		if(options.height && isNumber(options.height)) {
+		if (options.height && isNumber(options.height)) {
 			height = options.height + "px";
 		}
 
-		if(options.width && isNumber(options.width)) {
+		if (options.width && isNumber(options.width)) {
 			width = options.width + "px";
 		}
 
 		// Create new container element
-		let container = document.createElement("div");
-
+		const container = document.createElement("div");
 		container.id = this.id;
 		container.classList.add("epub-container");
 
 		// Style Element
-		// container.style.fontSize = "0";
 		container.style.wordSpacing = "0";
 		container.style.lineHeight = "0";
 		container.style.verticalAlign = "top";
 		container.style.position = "relative";
 
-		if(axis === "horizontal") {
-			// container.style.whiteSpace = "nowrap";
+		if (axis === "horizontal") {
 			container.style.display = "flex";
 			container.style.flexDirection = "row";
 			container.style.flexWrap = "nowrap";
 		}
 
-		if(width){
+		if (width) {
 			container.style.width = width;
 		}
 
-		if(height){
+		if (height) {
 			container.style.height = height;
 		}
 
@@ -104,13 +118,12 @@ class Stage {
 	 * @returns {Element} wrapper
 	 */
 	wrap(container) {
-		var wrapper = document.createElement("div");
 
+		const wrapper = document.createElement("div");
 		wrapper.style.visibility = "hidden";
 		wrapper.style.overflow = "hidden";
 		wrapper.style.width = "0";
 		wrapper.style.height = "0";
-
 		wrapper.appendChild(container);
 		return wrapper;
 	}
@@ -120,19 +133,16 @@ class Stage {
 	 * @param {Element|string} element 
 	 * @returns {Element}
 	 */
-	getElement(element){
-		var elm;
+	getElement(element) {
 
-		if(isElement(element)) {
+		let elm;
+		if (isElement(element)) {
 			elm = element;
 		} else if (typeof element === "string") {
 			elm = document.getElementById(element);
+		} else {
+			throw new TypeError("not valid argument type");
 		}
-
-		if(!elm){
-			throw new Error("Not an Element");
-		}
-
 		return elm;
 	}
 
@@ -141,27 +151,22 @@ class Stage {
 	 * @param {Element|string} what 
 	 * @returns {Element}
 	 */
-	attachTo(what){
+	attachTo(what) {
 
-		var element = this.getElement(what);
-		var base;
+		const element = this.getElement(what);
+		if (!element) return;
 
-		if(!element){
-			return;
-		}
-
-		if(this.settings.hidden) {
+		let base;
+		if (this.settings.hidden) {
 			base = this.wrapper;
 		} else {
 			base = this.container;
 		}
 
 		element.appendChild(base);
-
-		this.element = element;
+		this.parentElement = element;
 
 		return element;
-
 	}
 
 	/**
@@ -169,6 +174,7 @@ class Stage {
 	 * @returns {Element} container
 	 */
 	getContainer() {
+
 		return this.container;
 	}
 
@@ -176,42 +182,43 @@ class Stage {
 	 * onResize
 	 * @param {*} func 
 	 */
-	onResize(func){
+	onResize(func) {
 		// Only listen to window for resize event if width and height are not fixed.
 		// This applies if it is set to a percent or auto.
-		if(!isNumber(this.settings.width) ||
-			 !isNumber(this.settings.height) ) {
+		if (!isNumber(this.settings.width) ||
+			!isNumber(this.settings.height)) {
 			this.resizeFunc = throttle(func, 50);
 			window.addEventListener("resize", this.resizeFunc, false);
 		}
-
 	}
 
 	/**
 	 * onOrientationChange
 	 * @param {*} func 
 	 */
-	onOrientationChange(func){
+	onOrientationChange(func) {
+
 		this.orientationChangeFunc = func;
 		window.addEventListener("orientationchange", this.orientationChangeFunc, false);
 	}
 
 	/**
 	 * size
-	 * @param {string|number} width 
-	 * @param {string|number} height 
+	 * @param {string|number} [width] 
+	 * @param {string|number} [height] 
 	 * @returns {object}
 	 */
-	size(width, height){
-		var bounds;
+	size(width, height) {
+
+		let bounds;
 		let _width = width || this.settings.width;
 		let _height = height || this.settings.height;
 
 		// If width or height are set to false, inherit them from containing element
-		if(width === null) {
-			bounds = this.element.getBoundingClientRect();
+		if (width === null) {
+			bounds = this.parentElement.getBoundingClientRect();
 
-			if(bounds.width) {
+			if (bounds.width) {
 				width = Math.floor(bounds.width);
 				this.container.style.width = width + "px";
 			}
@@ -223,10 +230,10 @@ class Stage {
 			}
 		}
 
-		if(height === null) {
-			bounds = bounds || this.element.getBoundingClientRect();
+		if (height === null) {
+			bounds = bounds || this.parentElement.getBoundingClientRect();
 
-			if(bounds.height) {
+			if (bounds.height) {
 				height = bounds.height;
 				this.container.style.height = height + "px";
 			}
@@ -239,16 +246,15 @@ class Stage {
 			}
 		}
 
-		if(!isNumber(width)) {
+		if (!isNumber(width)) {
 			width = this.container.clientWidth;
 		}
 
-		if(!isNumber(height)) {
+		if (!isNumber(height)) {
 			height = this.container.clientHeight;
 		}
 
 		this.containerStyles = window.getComputedStyle(this.container);
-
 		this.containerPadding = {
 			left: parseFloat(this.containerStyles["padding-left"]) || 0,
 			right: parseFloat(this.containerStyles["padding-right"]) || 0,
@@ -257,7 +263,7 @@ class Stage {
 		};
 
 		// Bounds not set, get them from window
-		let _windowBounds = windowBounds();
+		let wndBounds = windowBounds();
 		let bodyStyles = window.getComputedStyle(document.body);
 		let bodyPadding = {
 			left: parseFloat(bodyStyles["padding-left"]) || 0,
@@ -267,77 +273,68 @@ class Stage {
 		};
 
 		if (!_width) {
-			width = _windowBounds.width -
-								bodyPadding.left -
-								bodyPadding.right;
+			width = wndBounds.width - bodyPadding.left - bodyPadding.right;
 		}
 
 		if ((this.settings.fullsize && !_height) || !_height) {
-			height = _windowBounds.height -
-								bodyPadding.top -
-								bodyPadding.bottom;
+			height = wndBounds.height - bodyPadding.top - bodyPadding.bottom;
 		}
 
 		return {
-			width: width -
-							this.containerPadding.left -
-							this.containerPadding.right,
-			height: height -
-							this.containerPadding.top -
-							this.containerPadding.bottom
-		};
-
+			width: width - this.containerPadding.left - this.containerPadding.right,
+			height: height - this.containerPadding.top - this.containerPadding.bottom
+		}
 	}
 
 	/**
-	 * bounds
-	 * @returns {object}
+	 * Get bounding client rect
+	 * @returns {DOMRect|object}
 	 */
-	bounds(){
+	bounds() {
+
 		let box;
 		if (this.container.style.overflow !== "visible") {
 			box = this.container && this.container.getBoundingClientRect();
 		}
 
-		if(!box || !box.width || !box.height) {
+		if (!box || !box.width || !box.height) {
 			return windowBounds();
 		} else {
 			return box;
 		}
-
 	}
 
 	/**
 	 * getSheet
 	 * @returns {CSSStyleSheet}
 	 */
-	getSheet(){
-		var style = document.createElement("style");
+	getSheet() {
 
+		const style = document.createElement("style");
 		// WebKit hack --> https://davidwalsh.name/add-rules-stylesheets
 		style.appendChild(document.createTextNode(""));
 
 		document.head.appendChild(style);
-
 		return style.sheet;
 	}
 
 	/**
 	 * addStyleRules
-	 * @param {*} selector 
+	 * @param {string} selector 
 	 * @param {object[]} rulesArray 
 	 */
-	addStyleRules(selector, rulesArray){
-		var scope = "#" + this.id + " ";
-		var rules = "";
+	addStyleRules(selector, rulesArray) {
 
-		if(!this.sheet){
+		let scope = "#" + this.id + " ";
+		let rules = "";
+
+		if (!this.sheet) {
 			this.sheet = this.getSheet();
 		}
 
-		rulesArray.forEach(function(set) {
-			for (var prop in set) {
-				if(set.hasOwnProperty(prop)) {
+		rulesArray.forEach(set => {
+			for (const prop in set) {
+				if (set.hasOwnProperty(prop)) {
 					rules += prop + ":" + set[prop] + ";";
 				}
 			}
@@ -351,7 +348,8 @@ class Stage {
 	 * @param {string} axis 
 	 */
 	axis(axis) {
-		if(axis === "horizontal") {
+
+		if (axis === "horizontal") {
 			this.container.style.display = "flex";
 			this.container.style.flexDirection = "row";
 			this.container.style.flexWrap = "nowrap";
@@ -361,21 +359,12 @@ class Stage {
 		this.settings.axis = axis;
 	}
 
-	// orientation(orientation) {
-	// 	if (orientation === "landscape") {
-	//
-	// 	} else {
-	//
-	// 	}
-	//
-	// 	this.orientation = orientation;
-	// }
-
 	/**
 	 * direction
 	 * @param {string} dir 
 	 */
 	direction(dir) {
+
 		if (this.container) {
 			this.container.dir = dir;
 			this.container.style["direction"] = dir;
@@ -392,6 +381,7 @@ class Stage {
 	 * @param {string} overflow 
 	 */
 	overflow(overflow) {
+
 		if (this.container) {
 			if (overflow === "scroll" && this.settings.axis === "vertical") {
 				this.container.style["overflow-y"] = overflow;
@@ -410,23 +400,22 @@ class Stage {
 	 * destroy
 	 */
 	destroy() {
-		var base;
 
-		if (this.element) {
+		if (this.parentElement) {
 
-			if(this.settings.hidden) {
+			let base;
+			if (this.settings.hidden) {
 				base = this.wrapper;
 			} else {
 				base = this.container;
 			}
 
-			if(this.element.contains(this.container)) {
-				this.element.removeChild(this.container);
+			if (this.parentElement.contains(base)) {
+				this.parentElement.removeChild(base);
 			}
 
 			window.removeEventListener("resize", this.resizeFunc);
 			window.removeEventListener("orientationChange", this.orientationChangeFunc);
-
 		}
 	}
 }
