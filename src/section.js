8f82cc3bacb05a568f12315651bcd4d6ae45137c
vite <nuget@list.ru>
2024-05-12 02:13:47 +0300

    refactoring src/section.js

--- src/section.js
+++ src/section.js
@@ -1,31 +1,81 @@
-import { defer } from "./utils/core";
 import EpubCFI from "./epubcfi";
 import Hook from "./utils/hook";
-import { sprint } from "./utils/core";
-import { replaceBase } from "./utils/replacements";
 import Request from "./utils/request";
+import { defer, sprint } from "./utils/core";
+import { replaceBase } from "./utils/replacements";
 import { DOMParser as XMLDOMSerializer } from "@xmldom/xmldom";
 
 /**
  * Represents a Section of the Book
- *
  * In most books this is equivalent to a Chapter
- * @param {object} item  The spine item representing the section
- * @param {object} hooks hooks for serialize and content
  */
 class Section {
-	constructor(item, hooks){
+	/**
+	 * Constructor
+	 * @param {object} item 
+	 * @param {object} hooks 
+	 */
+	constructor(item, hooks) {
+		/**
+		 * @member {string} idref
+		 * @memberof Section
+		 * @readonly
+		 */
 		this.idref = item.idref;
+		/**
+		 * @member {boolean} linear
+		 * @memberof Section
+		 * @readonly
+		 */
 		this.linear = item.linear === "yes";
-		this.properties = item.properties;
+		/**
+		 * @member {number} index
+		 * @memberof Section
+		 * @readonly
+		 */
 		this.index = item.index;
+		/**
+		 * @member {string} href
+		 * @memberof Section
+		 * @readonly
+		 */
 		this.href = item.href;
+		/**
+		 * @member {string} url
+		 * @memberof Section
+		 * @readonly
+		 */
 		this.url = item.url;
+		/**
+		 * @member {string} canonical
+		 * @memberof Section
+		 * @readonly
+		 */
 		this.canonical = item.canonical;
-		this.next = item.next;
-		this.prev = item.prev;
-
+		/**
+		 * @member {string} cfiBase
+		 * @memberof Section
+		 * @readonly
+		 */
 		this.cfiBase = item.cfiBase;
+		/**
+		 * @member {function} next
+		 * @memberof Section
+		 * @readonly
+		 */
+		this.next = item.next;
+		/**
+		 * @member {function} prev
+		 * @memberof Section
+		 * @readonly
+		 */
+		this.prev = item.prev;
+		/**
+		 * @member {object[]} properties
+		 * @memberof Section
+		 * @readonly
+		 */
+		this.properties = item.properties;
 
 		if (hooks) {
 			this.hooks = hooks;
@@ -42,118 +92,111 @@ class Section {
 
 	/**
 	 * Load the section from its url
-	 * @param  {method} [_request] a request method to use for loading
-	 * @return {document} a promise with the xml document
+	 * @param {function} [req] a request method to use for loading
+	 * @return {Promise} a promise with the xml document
 	 */
-	load(_request){
-		var request = _request || this.request || Request;
-		var loading = new defer();
-		var loaded = loading.promise;
+	load(req) {
 
-		if(this.contents) {
+		const request = req || this.request || Request;
+		const loading = new defer();
+		const loaded = loading.promise;
+
+		if (this.contents) {
 			loading.resolve(this.contents);
 		} else {
-			request(this.url)
-				.then(function(xml){
-					// var directory = new Url(this.url).directory;
-
-					this.document = xml;
-					this.contents = xml.documentElement;
-
-					return this.hooks.content.trigger(this.document, this);
-				}.bind(this))
-				.then(function(){
-					loading.resolve(this.contents);
-				}.bind(this))
-				.catch(function(error){
-					loading.reject(error);
-				});
+			request(this.url).then((xml) => {
+				this.document = xml;
+				this.contents = xml.documentElement;
+				return this.hooks.content.trigger(this.document, this);
+			}).then(() => {
+				loading.resolve(this.contents);
+			}).catch((error) => {
+				loading.reject(error);
+			});
 		}
 
 		return loaded;
 	}
 
 	/**
-	 * Adds a base tag for resolving urls in the section
+	 * Adds a base tag for resolving urls in the section (unused)
 	 * @private
 	 */
-	base(){
+	base() {
+
 		return replaceBase(this.document, this);
 	}
 
 	/**
 	 * Render the contents of a section
-	 * @param  {method} [_request] a request method to use for loading
+	 * @param {function} [request] a request method to use for loading
 	 * @return {string} output a serialized XML Document
 	 */
-	render(_request){
-		var rendering = new defer();
-		var rendered = rendering.promise;
-		this.output; // TODO: better way to return this from hooks?
+	render(request) {
 
-		this.load(_request).
-			then(function(contents){
-				var userAgent = (typeof navigator !== 'undefined' && navigator.userAgent) || '';
-				var isIE = userAgent.indexOf('Trident') >= 0;
-				var Serializer;
-				if (typeof XMLSerializer === "undefined" || isIE) {
-					Serializer = XMLDOMSerializer;
-				} else {
-					Serializer = XMLSerializer;
-				}
-				var serializer = new Serializer();
-				this.output = serializer.serializeToString(contents);
-				return this.output;
-			}.bind(this)).
-			then(function(){
-				return this.hooks.serialize.trigger(this.output, this);
-			}.bind(this)).
-			then(function(){
-				rendering.resolve(this.output);
-			}.bind(this))
-			.catch(function(error){
-				rendering.reject(error);
-			});
+		const rendering = new defer();
+		const rendered = rendering.promise;
+		this.output; // TODO: better way to return this from hooks?
+		this.load(request).then((contents) => {
+
+			const userAgent = (typeof navigator !== "undefined" && navigator.userAgent) || "";
+			const isIE = userAgent.indexOf("Trident") >= 0;
+			let Serializer;
+			if (typeof XMLSerializer === "undefined" || isIE) {
+				Serializer = XMLDOMSerializer;
+			} else {
+				Serializer = XMLSerializer;
+			}
+			const serializer = new Serializer();
+			this.output = serializer.serializeToString(contents);
+			return this.output;
+		}).then(() => {
+			return this.hooks.serialize.trigger(this.output, this);
+		}).then(() => {
+			rendering.resolve(this.output);
+		}).catch((error) => {
+			rendering.reject(error);
+		});
 
 		return rendered;
 	}
 
 	/**
 	 * Find a string in a section
-	 * @param  {string} _query The query string to find
+	 * @param {string} query The query string to find
 	 * @return {object[]} A list of matches, with form {cfi, excerpt}
 	 */
-	find(_query){
-		var section = this;
-		var matches = [];
-		var query = _query.toLowerCase();
-		var find = function(node){
-			var text = node.textContent.toLowerCase();
-			var range = section.document.createRange();
-			var cfi;
-			var pos;
-			var last = -1;
-			var excerpt;
-			var limit = 150;
+	find(query) {
 
-			while (pos != -1) {
+		const section = this;
+		const matches = [];
+		const q = query.toLowerCase();
+		const find = (node) => {
+
+			const text = node.textContent.toLowerCase();
+			const range = section.document.createRange();
+			const limit = 150;
+			let pos, last = -1;
+
+			while (pos !== -1) {
 				// Search for the query
-				pos = text.indexOf(query, last + 1);
+				pos = text.indexOf(q, last + 1);
 
-				if (pos != -1) {
+				if (pos !== -1) {
 					// We found it! Generate a CFI
 					range = section.document.createRange();
 					range.setStart(node, pos);
-					range.setEnd(node, pos + query.length);
+					range.setEnd(node, pos + q.length);
 
-					cfi = section.cfiFromRange(range);
+					const cfi = section.cfiFromRange(range);
 
+					let excerpt;
 					// Generate the excerpt
 					if (node.textContent.length < limit) {
 						excerpt = node.textContent;
 					}
 					else {
-						excerpt = node.textContent.substring(pos - limit/2, pos + limit/2);
+						excerpt = node.textContent.substring(pos - limit / 2, pos + limit / 2);
 						excerpt = "..." + excerpt + "...";
 					}
 
@@ -166,59 +209,63 @@ class Section {
 
 				last = pos;
 			}
-		};
+		}
 
-		sprint(section.document, function(node) {
-			find(node);
-		});
+		sprint(section.document, (node) => find(node));
 
 		return matches;
-	};
-
+	}
 
 	/**
-	 * Search a string in multiple sequential Element of the section. If the document.createTreeWalker api is missed(eg: IE8), use `find` as a fallback.
-	 * @param  {string} _query The query string to search
-	 * @param  {int} maxSeqEle The maximum number of Element that are combined for search, default value is 5.
+	 * Search a string in multiple sequential Element of the section.
+	 * If the document.createTreeWalker api is missed(eg: IE8), use 
+	 * `find` as a fallback.
+	 * @param {string} query The query string to search
+	 * @param {number} [maxSeqEle=5] The maximum number of Element that are combined for search, default value is 5.
 	 * @return {object[]} A list of matches, with form {cfi, excerpt}
 	 */
-	search(_query , maxSeqEle = 5){
-		if (typeof(document.createTreeWalker) == "undefined") {
-			return this.find(_query);
+	search(query, maxSeqEle = 5) {
+
+		if (typeof (document.createTreeWalker) == "undefined") {
+			return this.find(query);
 		}
-		let matches = [];
+		const matches = [];
 		const excerptLimit = 150;
 		const section = this;
-		const query = _query.toLowerCase();
-		const search = function(nodeList){
-			const textWithCase =  nodeList.reduce((acc ,current)=>{
+		const q = query.toLowerCase();
+		const search = (nodeList) => {
+			const textWithCase = nodeList.reduce((acc, current) => {
 				return acc + current.textContent;
-			},"");
+			}, "");
 			const text = textWithCase.toLowerCase();
-			const pos = text.indexOf(query);
-			if (pos != -1){
-				const startNodeIndex = 0 , endPos = pos + query.length;
-				let endNodeIndex = 0 , l = 0;
-				if (pos < nodeList[startNodeIndex].length){
-					let cfi;
-					while( endNodeIndex < nodeList.length - 1 ){
-						l += nodeList[endNodeIndex].length;
-						if ( endPos <= l){
+			const pos = text.indexOf(q);
+			if (pos !== -1) {
+				const startNodeIndex = 0, endPos = pos + q.length;
+				let endNodeIndex = 0, len = 0;
+				if (pos < nodeList[startNodeIndex].length) {
+					while (endNodeIndex < nodeList.length - 1) {
+						len += nodeList[endNodeIndex].length;
+						if (endPos <= len) {
 							break;
 						}
 						endNodeIndex += 1;
 					}
 
-					let startNode = nodeList[startNodeIndex] , endNode = nodeList[endNodeIndex];
-					let range = section.document.createRange();
-					range.setStart(startNode,pos);
-					let beforeEndLengthCount =  nodeList.slice(0, endNodeIndex).reduce((acc,current)=>{return acc+current.textContent.length;},0) ;
-					range.setEnd(endNode, beforeEndLengthCount > endPos ? endPos : endPos - beforeEndLengthCount );
-					cfi = section.cfiFromRange(range);
+					const startNode = nodeList[startNodeIndex];
+					const endNode = nodeList[endNodeIndex];
+					const range = section.document.createRange();
+					range.setStart(startNode, pos);
+					const beforeEndLengthCount = nodeList.slice(0, endNodeIndex).reduce((acc, current) => {
+						return acc + current.textContent.length;
+					}, 0);
+					range.setEnd(endNode, beforeEndLengthCount > endPos ? endPos : endPos - beforeEndLengthCount);
+					const cfi = section.cfiFromRange(range);
 
-					let excerpt = nodeList.slice(0, endNodeIndex+1).reduce((acc,current)=>{return acc+current.textContent ;},"");
-					if (excerpt.length > excerptLimit){
-						excerpt = excerpt.substring(pos - excerptLimit/2, pos + excerptLimit/2);
+					let excerpt = nodeList.slice(0, endNodeIndex + 1).reduce((acc, current) => {
+						return acc + current.textContent;
+					}, "");
+					if (excerpt.length > excerptLimit) {
+						excerpt = excerpt.substring(pos - excerptLimit / 2, pos + excerptLimit / 2);
 						excerpt = "..." + excerpt + "...";
 					}
 					matches.push({
@@ -230,15 +277,15 @@ class Section {
 		}
 
 		const treeWalker = document.createTreeWalker(section.document, NodeFilter.SHOW_TEXT, null, false);
-		let node , nodeList = [];
+		let node, nodeList = [];
 		while (node = treeWalker.nextNode()) {
 			nodeList.push(node);
-			if (nodeList.length == maxSeqEle){
-				search(nodeList.slice(0 , maxSeqEle));
+			if (nodeList.length == maxSeqEle) {
+				search(nodeList.slice(0, maxSeqEle));
 				nodeList = nodeList.slice(1, maxSeqEle);
 			}
 		}
-		if (nodeList.length > 0){
+		if (nodeList.length > 0) {
 			search(nodeList);
 		}
 		return matches;
@@ -247,48 +294,49 @@ class Section {
 	/**
 	* Reconciles the current chapters layout properties with
 	* the global layout properties.
-	* @param {object} globalLayout  The global layout settings object, chapter properties string
+	* @param {object} globalLayout The global layout settings object, chapter properties string
 	* @return {object} layoutProperties Object with layout properties
 	*/
-	reconcileLayoutSettings(globalLayout){
+	reconcileLayoutSettings(globalLayout) {
 		//-- Get the global defaults
-		var settings = {
-			layout : globalLayout.layout,
-			spread : globalLayout.spread,
-			orientation : globalLayout.orientation
+		const settings = {
+			layout: globalLayout.layout,
+			spread: globalLayout.spread,
+			orientation: globalLayout.orientation
 		};
 
 		//-- Get the chapter's display type
-		this.properties.forEach(function(prop){
-			var rendition = prop.replace("rendition:", "");
-			var split = rendition.indexOf("-");
-			var property, value;
+		this.properties.forEach(prop => {
+			const rendition = prop.replace("rendition:", "");
+			const split = rendition.indexOf("-");
 
-			if(split != -1){
-				property = rendition.slice(0, split);
-				value = rendition.slice(split+1);
-
+			if (split !== -1) {
+				const property = rendition.slice(0, split);
+				const value = rendition.slice(split + 1);
 				settings[property] = value;
 			}
 		});
+
 		return settings;
 	}
 
 	/**
 	 * Get a CFI from a Range in the Section
-	 * @param  {range} _range
+	 * @param {range} range
 	 * @return {string} cfi an EpubCFI string
 	 */
-	cfiFromRange(_range) {
-		return new EpubCFI(_range, this.cfiBase).toString();
+	cfiFromRange(range) {
+
+		return new EpubCFI(range, this.cfiBase).toString();
 	}
 
 	/**
 	 * Get a CFI from an Element in the Section
-	 * @param  {element} el
+	 * @param {element} el
 	 * @return {string} cfi an EpubCFI string
 	 */
 	cfiFromElement(el) {
+
 		return new EpubCFI(el, this.cfiBase).toString();
 	}
 
@@ -296,16 +344,20 @@ class Section {
 	 * Unload the section document
 	 */
 	unload() {
+
 		this.document = undefined;
 		this.contents = undefined;
 		this.output = undefined;
 	}
 
+	/**
+	 * destroy
+	 */
 	destroy() {
+
 		this.unload();
 		this.hooks.serialize.clear();
 		this.hooks.content.clear();
-
 		this.hooks = undefined;
 		this.idref = undefined;
 		this.linear = undefined;
@@ -315,7 +367,6 @@ class Section {
 		this.url = undefined;
 		this.next = undefined;
 		this.prev = undefined;
-
 		this.cfiBase = undefined;
 	}
 }
