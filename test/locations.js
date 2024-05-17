1d26abbac57137c111aa6fedeff53eaf4ffe73d2
vite <nuget@list.ru>
2024-05-17 04:21:41 +0300

    added locations.generate test

--- test/locations.js
+++ test/locations.js
@@ -1,4 +1,5 @@
 import assert from "assert"
+import Book from "../src/book"
 import Locations from "../src/locations"
 import * as core from "../src/utils/core"
 import chapter from "./fixtures/locations.xhtml"
@@ -18,6 +19,15 @@ describe("Locations", () => {
 			const locations = new Locations()
 			const result = locations.parse(contents, "/6/4[chap01ref]", 100)
 			assert.equal(result.length, 15)
+		})
+	})
+	describe("#generate", () => {
+		const book = new Book("/fixtures/alice/OPS/package.opf")
+		it("generate locations", async () => {
+			await book.opened
+			await book.locations.generate(826).then((locations) => {
+				assert.equal(locations.length, 68)
+			})
 		})
 	})
 })
