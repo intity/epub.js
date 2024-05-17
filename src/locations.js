253a8ad343e25d65a909cebdc4fbcb1475e76033
vite <nuget@list.ru>
2024-05-17 04:45:22 +0300

    removed generator from words

--- src/locations.js
+++ src/locations.js
@@ -22,10 +22,8 @@ class Locations extends Array {
 		this.pause = pause || 100;
 		this.q = new Queue(this);
 		this.epubcfi = new EpubCFI();
-		this.words = [];
 		this.total = 0;
 		this.break = 150;
-		this._wordCounter = 0;
 		this.current = 0;
 		this.currentCfi = "";
 		this.currentLocation = "";
@@ -177,166 +175,6 @@ class Locations extends Array {
 			locations.push(cfi);
 			counter = 0;
 		}
-
-		return locations;
-	}
-
-	/**
-	 * Load all of sections in the book to generate locations
-	 * @param  {string} startCfi start position
-	 * @param  {int} wordCount how many words to split on
-	 * @param  {int} count result count
-	 * @return {object} locations
-	 */
-	generateFromWords(startCfi, wordCount, count) {
-
-		const start = startCfi ? new EpubCFI(startCfi) : undefined;
-		this.q.pause();
-		this.words = [];
-		this._wordCounter = 0;
-		this.spine.each(section => {
-			if (section.linear) {
-				if (start) {
-					if (section.index >= start.spinePos) {
-						this.q.enqueue(this.processWords.bind(this), section, wordCount, start, count);
-					}
-				} else {
-					this.q.enqueue(this.processWords.bind(this), section, wordCount, start, count);
-				}
-			}
-		});
-
-		return this.q.run().then(() => {
-
-			if (this.currentCfi) {
-				this.currentLocation = this.currentCfi;
-			}
-
-			return this.words;
-		});
-	}
-
-	/**
-	 * processWords
-	 * @param {Section} section 
-	 * @param {number} wordCount 
-	 * @param {*} startCfi 
-	 * @param {number} [count] 
-	 * @returns {Promise}
-	 */
-	async processWords(section, wordCount, startCfi, count) {
-
-		if (count && this.words.length >= count) {
-			return Promise.resolve();
-		}
-
-		return section.load(this.request).then((contents) => {
-			const completed = new defer();
-			const locations = this.parseWords(contents, section, wordCount, startCfi);
-			const remainingCount = count - this.words.length;
-			this.words = this.words.concat(locations.length >= count ? locations.slice(0, remainingCount) : locations);
-
-			section.unload();
-
-			this.processingTimeout = setTimeout(() => completed.resolve(locations), this.pause);
-			return completed.promise;
-		});
-	}
-
-	/**
-	 * countWords
-	 * 
-	 * http://stackoverflow.com/questions/18679576/counting-words-in-string
-	 * @param {string} s 
-	 * @returns 
-	 */
-	countWords(s) {
-		s = s.replace(/(^\s*)|(\s*$)/gi, "");//exclude  start and end white-space
-		s = s.replace(/[ ]{2,}/gi, " ");//2 or more space to 1
-		s = s.replace(/\n /, "\n"); // exclude newline with a start spacing
-		return s.split(" ").length;
-	}
-
-	/**
-	 * parseWords
-	 * @param {Element} contents 
-	 * @param {Section} section 
-	 * @param {number} wordCount 
-	 * @param {*} [startCfi] 
-	 * @returns {object[]}
-	 */
-	parseWords(contents, section, wordCount, startCfi) {
-
-		const cfiBase = section.cfiBase;
-		const locations = [];
-		const doc = contents.ownerDocument;
-		const body = qs(doc, "body");
-		let prev;
-		let foundStartNode = startCfi ? startCfi.spinePos !== section.index : true;
-		let startNode;
-		if (startCfi && section.index === startCfi.spinePos) {
-			startNode = startCfi.findNode(startCfi.range ? startCfi.path.steps.concat(startCfi.start.steps) : startCfi.path.steps, contents.ownerDocument);
-		}
-		const parser = (node) => {
-
-			if (!foundStartNode) {
-				if (node === startNode) {
-					foundStartNode = true;
-				} else {
-					return false;
-				}
-			}
-
-			if (node.textContent.length < 10) {
-				if (node.textContent.trim().length === 0) {
-					return false;
-				}
-			}
-
-			const len = this.countWords(node.textContent);
-
-			if (len === 0) {
-				return false; // continue
-			}
-
-			const _break = wordCount;
-			let dist = _break - this._wordCounter;
-			let pos = 0;
-
-			// Node is smaller than a break,
-			// skip over it
-			if (dist > len) {
-				this._wordCounter += len;
-				pos = len;
-			}
-
-
-			while (pos < len) {
-				dist = _break - this._wordCounter;
-
-				// Gone over
-				if (pos + dist >= len) {
-					// Continue counter for next node
-					this._wordCounter += len - pos;
-					// break
-					pos = len;
-					// At End
-				} else {
-					// Advance pos
-					pos += dist;
-
-					let cfi = new EpubCFI(node, cfiBase);
-					locations.push({
-						cfi: cfi.toString(),
-						wordCount: this._wordCounter
-					});
-					this._wordCounter = 0;
-				}
-			}
-			prev = node;
-		};
-
-		sprint(body, parser.bind(this));
 
 		return locations;
 	}
@@ -519,21 +357,17 @@ class Locations extends Array {
 	destroy() {
 
 		this.spine = undefined;
-		this.request = undefined;
 		this.pause = undefined;
-
+		this.break = undefined;
+		this.request = undefined;
 		this.q.stop();
 		this.q = undefined;
 		this.epubcfi = undefined;
-
 		this.splice(0);
 		this.total = undefined;
-
-		this.break = undefined;
 		this.current = undefined;
-
-		this.currentLocation = undefined;
 		this.currentCfi = undefined;
+		this.currentLocation = undefined;
 		clearTimeout(this.processingTimeout);
 	}
 }
