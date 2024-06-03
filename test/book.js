658d0622d348d593bc8967320f29312e4b2833af
vite <nuget@list.ru>
2024-06-03 20:57:48 +0300

    updating test/book.js

--- test/book.js
+++ test/book.js
@@ -2,29 +2,31 @@ import assert from "assert"
 import Book from "../src/book"
 
 describe("Book", () => {
-	describe("Unarchived: open from directory", () => {
+	describe("Unarchived: open book from directory", () => {
 		const book = new Book("/fixtures/alice/")
 		it ("should open a epub", async () => {
 			await book.opened
+			assert.equal(book.isOpen, true, "book is opened")
+			assert.equal(book.url.toString(), "http://localhost:9876/fixtures/alice/", "book url is passed to new Book instance")
 			assert.equal(book.container.directory, "OPS")
 			assert.equal(book.container.fullPath, "OPS/package.opf")
 			assert.equal(book.container.encoding, "UTF-8")
 			assert.equal(book.container.mediaType, "application/oebps-package+xml")
 		})
 	})
-	describe("Unarchived: open from package.opf", () => {
+	describe("Unarchived: open book from package.opf", () => {
 		const book = new Book("/fixtures/alice/OPS/package.opf")
 		it("should open a epub", async () => {
 			await book.opened
 			assert.equal(book.isOpen, true, "book is opened")
-			assert.equal(book.url.toString(), "http://localhost:9876/fixtures/alice/OPS/package.opf", "book url is passed to new Book")
+			assert.equal(book.url.toString(), "http://localhost:9876/fixtures/alice/OPS/package.opf", "book url is passed to new Book instance")
 		})
 		it("should have a local coverUrl", async () => {
 			const coverUrl = await book.coverUrl()
 			assert.equal(coverUrl, "http://localhost:9876/fixtures/alice/OPS/images/cover_th.jpg", "cover url is available")
 		})
 	})
-	describe("Archived epub", () => {
+	describe("Archived: open book from epub file", () => {
 		const book = new Book("/fixtures/alice.epub")
 		it("should open a archived epub", async () => {
 			await book.opened
@@ -36,7 +38,7 @@ describe("Book", () => {
 			assert(/^blob:http:\/\/localhost:9876\/[^\/]+$/.test(coverUrl), "cover url is available and a blob: url")
 		})
 	})
-	describe("Archived epub in array buffer without options", () => {
+	describe("Archived: open book from array buffer without options", () => {
 		let book
 		before(async () => {
 			const response = await fetch("/fixtures/alice.epub")
@@ -53,7 +55,7 @@ describe("Book", () => {
 			assert(/^blob:http:\/\/localhost:9876\/[^\/]+$/.test(coverUrl), "cover url is available and a blob: url")
 		})
 	})
-	describe("Archived epub without cover", () => {
+	describe("Archived: open book from epub file without cover", () => {
 		const book = new Book("/fixtures/alice_without_cover.epub")
 		it("should open a archived epub", async () => {
 			await book.opened
