cfbec4912d867dbad240fe52dd1252a6153a1243
vite <nuget@list.ru>
2024-06-12 00:09:55 +0300

    refactoring src/pagelist.js

--- src/pagelist.js
+++ src/pagelist.js
@@ -9,10 +9,14 @@ import {
 
 /**
  * Page List Parser
- * @param {document} [xml]
  */
 class PageList {
+	/**
+	 * Constructor
+	 * @param {Document} [xml] 
+	 */
 	constructor(xml) {
+
 		this.pages = [];
 		this.locations = [];
 		this.epubcfi = new EpubCFI();
@@ -28,162 +32,166 @@ class PageList {
 			this.pageList = this.parse(xml);
 		}
 
-		if(this.pageList && this.pageList.length) {
+		if (this.pageList && this.pageList.length) {
 			this.process(this.pageList);
 		}
 	}
 
 	/**
 	 * Parse PageList Xml
-	 * @param  {document} xml
+	 * @param {Document} xml
+	 * @returns {Array}
 	 */
 	parse(xml) {
-		var html = qs(xml, "html");
-		var ncx = qs(xml, "ncx");
 
-		if(html) {
+		const html = qs(xml, "html");
+		const ncx = qs(xml, "ncx");
+
+		if (html) {
 			return this.parseNav(xml);
-		} else if(ncx){
+		} else if (ncx) {
 			return this.parseNcx(xml);
 		}
 
+		return [];
 	}
 
 	/**
 	 * Parse a Nav PageList
-	 * @private
-	 * @param  {node} navHtml
+	 * @param {Node} navHtml
 	 * @return {PageList.item[]} list
+	 * @private
 	 */
-	parseNav(navHtml){
-		var navElement = querySelectorByType(navHtml, "nav", "page-list");
-		var navItems = navElement ? qsa(navElement, "li") : [];
-		var length = navItems.length;
-		var i;
-		var list = [];
-		var item;
+	parseNav(navHtml) {
 
-		if(!navItems || length === 0) return list;
+		const navElement = querySelectorByType(navHtml, "nav", "page-list");
+		const navItems = navElement ? qsa(navElement, "li") : [];
+		const length = navItems.length;
+		const list = [];
 
-		for (i = 0; i < length; ++i) {
-			item = this.item(navItems[i]);
+		if (!navItems || length === 0) return list;
+
+		for (let i = 0; i < length; ++i) {
+			const item = this.item(navItems[i]);
 			list.push(item);
 		}
 
 		return list;
 	}
 
+	/**
+	 * parseNcx
+	 * @param {Node} navXml 
+	 * @returns {Array}
+	 * @private
+	 */
 	parseNcx(navXml) {
-		var list = [];
-		var i = 0;
-		var item;
-		var pageList;
-		var pageTargets;
-		var length = 0;
 
-		pageList = qs(navXml, "pageList");
+		const list = [];
+		const pageList = qs(navXml, "pageList");
+
 		if (!pageList) return list;
 
-		pageTargets = qsa(pageList, "pageTarget");
-		length = pageTargets.length;
+		const pageTargets = qsa(pageList, "pageTarget");
+		const length = pageTargets.length;
 
 		if (!pageTargets || pageTargets.length === 0) {
 			return list;
 		}
 
-		for (i = 0; i < length; ++i) {
-			item = this.ncxItem(pageTargets[i]);
+		for (let i = 0; i < length; ++i) {
+			const item = this.ncxItem(pageTargets[i]);
 			list.push(item);
 		}
 
 		return list;
 	}
 
+	/**
+	 * ncxItem
+	 * @param {Node} item 
+	 * @returns {object}
+	 * @private
+	 */
 	ncxItem(item) {
-		var navLabel = qs(item, "navLabel");
-		var navLabelText = qs(navLabel, "text");
-		var pageText = navLabelText.textContent;
-		var content = qs(item, "content");
 
-		var href = content.getAttribute("src");
-		var page = parseInt(pageText, 10);
+		const navLabel = qs(item, "navLabel");
+		const navLabelText = qs(navLabel, "text");
+		const pageText = navLabelText.textContent;
+		const content = qs(item, "content");
 
 		return {
-			"href": href,
-			"page": page,
+			href: content.getAttribute("src"),
+			page: parseInt(pageText, 10)
 		};
 	}
 
 	/**
 	 * Page List Item
-	 * @private
-	 * @param  {node} item
+	 * @param {Node} item
 	 * @return {object} pageListItem
+	 * @private
 	 */
-	item(item){
-		var content = qs(item, "a"),
-				href = content.getAttribute("href") || "",
-				text = content.textContent || "",
-				page = parseInt(text),
-				isCfi = href.indexOf("epubcfi"),
-				split,
-				packageUrl,
-				cfi;
+	item(item) {
 
-		if(isCfi != -1) {
-			split = href.split("#");
-			packageUrl = split[0];
-			cfi = split.length > 1 ? split[1] : false;
+		const content = qs(item, "a");
+		const href = content.getAttribute("href") || "";
+		const text = content.textContent || "";
+		const page = parseInt(text);
+
+		if (href.indexOf("epubcfi") !== -1) {
+			const split = href.split("#");
 			return {
-				"cfi" : cfi,
-				"href" : href,
-				"packageUrl" : packageUrl,
-				"page" : page
+				cfi: split.length > 1 ? split[1] : false,
+				href: href,
+				packageUrl: split[0],
+				page: page
 			};
 		} else {
 			return {
-				"href" : href,
-				"page" : page
+				href: href,
+				page: page
 			};
 		}
 	}
 
 	/**
 	 * Process pageList items
+	 * @param {array} pageList
 	 * @private
-	 * @param  {array} pageList
 	 */
-	process(pageList){
-		pageList.forEach(function(item){
+	process(pageList) {
+
+		pageList.forEach((item) => {
 			this.pages.push(item.page);
 			if (item.cfi) {
 				this.locations.push(item.cfi);
 			}
 		}, this);
 		this.firstPage = parseInt(this.pages[0]);
-		this.lastPage = parseInt(this.pages[this.pages.length-1]);
+		this.lastPage = parseInt(this.pages[this.pages.length - 1]);
 		this.totalPages = this.lastPage - this.firstPage;
 	}
 
 	/**
 	 * Get a PageList result from a EpubCFI
-	 * @param  {string} cfi EpubCFI String
+	 * @param {string} cfi EpubCFI String
 	 * @return {number} page
 	 */
-	pageFromCfi(cfi){
-		var pg = -1;
-
+	pageFromCfi(cfi) {
 		// Check if the pageList has not been set yet
-		if(this.locations.length === 0) {
+		if (this.locations.length === 0) {
 			return -1;
 		}
-
 		// TODO: check if CFI is valid?
 
 		// check if the cfi is in the location list
 		// var index = this.locations.indexOf(cfi);
-		var index = indexOfSorted(cfi, this.locations, this.epubcfi.compare);
-		if(index != -1) {
+		let pg, index = indexOfSorted(cfi,
+			this.locations,
+			this.epubcfi.compare
+		);
+		if (index != -1) {
 			pg = this.pages[index];
 		} else {
 			// Otherwise add it to the list of locations
@@ -191,34 +199,33 @@ class PageList {
 			//index = EPUBJS.core.insert(cfi, this.locations, this.epubcfi.compare);
 			index = locationOf(cfi, this.locations, this.epubcfi.compare);
 			// Get the page at the location just before the new one, or return the first
-			pg = index-1 >= 0 ? this.pages[index-1] : this.pages[0];
-			if(pg !== undefined) {
+			pg = index - 1 >= 0 ? this.pages[index - 1] : this.pages[0];
+			if (pg !== undefined) {
 				// Add the new page in so that the locations and page array match up
 				//this.pages.splice(index, 0, pg);
 			} else {
 				pg = -1;
 			}
-
 		}
 		return pg;
 	}
 
 	/**
 	 * Get an EpubCFI from a Page List Item
-	 * @param  {string | number} pg
+	 * @param {string|number} pg
 	 * @return {string} cfi
 	 */
-	cfiFromPage(pg){
-		var cfi = -1;
+	cfiFromPage(pg) {
 		// check that pg is an int
-		if(typeof pg != "number"){
+		if (typeof pg !== "number") {
 			pg = parseInt(pg);
 		}
 
 		// check if the cfi is in the page list
 		// Pages could be unsorted.
-		var index = this.pages.indexOf(pg);
-		if(index != -1) {
+		const index = this.pages.indexOf(pg);
+		let cfi = -1;
+		if (index !== -1) {
 			cfi = this.locations[index];
 		}
 		// TODO: handle pages not in the list
@@ -227,32 +234,34 @@ class PageList {
 
 	/**
 	 * Get a Page from Book percentage
-	 * @param  {number} percent
+	 * @param {number} percent
 	 * @return {number} page
 	 */
-	pageFromPercentage(percent){
-		var pg = Math.round(this.totalPages * percent);
-		return pg;
+	pageFromPercentage(percent) {
+
+		return Math.round(this.totalPages * percent);
 	}
 
 	/**
 	 * Returns a value between 0 - 1 corresponding to the location of a page
-	 * @param  {number} pg the page
+	 * @param {number} pg the page
 	 * @return {number} percentage
 	 */
-	percentageFromPage(pg){
-		var percentage = (pg - this.firstPage) / this.totalPages;
+	percentageFromPage(pg) {
+
+		const percentage = (pg - this.firstPage) / this.totalPages;
 		return Math.round(percentage * 1000) / 1000;
 	}
 
 	/**
 	 * Returns a value between 0 - 1 corresponding to the location of a cfi
-	 * @param  {string} cfi EpubCFI String
+	 * @param {string} cfi EpubCFI String
 	 * @return {number} percentage
 	 */
-	percentageFromCfi(cfi){
-		var pg = this.pageFromCfi(cfi);
-		var percentage = this.percentageFromPage(pg);
+	percentageFromCfi(cfi) {
+
+		const pg = this.pageFromCfi(cfi);
+		const percentage = this.percentageFromPage(pg);
 		return percentage;
 	}
 
@@ -260,12 +269,11 @@ class PageList {
 	 * Destroy
 	 */
 	destroy() {
+
 		this.pages = undefined;
 		this.locations = undefined;
 		this.epubcfi = undefined;
-
 		this.pageList = undefined;
-
 		this.toc = undefined;
 		this.ncx = undefined;
 	}
