1b83fa6afa86f9f448e81ca0618d0885e294beab
vite <nuget@list.ru>
2024-06-13 10:47:59 +0300

    refactoring src/utils/hook.js

--- src/utils/hook.js
+++ src/utils/hook.js
@@ -2,26 +2,31 @@
  * Hooks allow for injecting functions that must all complete in order before finishing
  * They will execute in parallel but all must finish before continuing
  * Functions may return a promise if they are async.
- * @param {any} context scope of this
- * @example this.content = new EPUBJS.Hook(this);
  */
 class Hook {
-	constructor(context){
+	/**
+	 * Constructor
+	 * @param {any} context scope of this
+	 * @example this.content = new Hook(this);
+	 */
+	constructor(context) {
+
 		this.context = context || this;
 		this.hooks = [];
 	}
 
 	/**
 	 * Adds a function to be run before a hook completes
-	 * @example this.content.register(function(){...});
+	 * @example this.content.register(() => {...});
 	 */
-	register(){
-		for(var i = 0; i < arguments.length; ++i) {
-			if (typeof arguments[i]  === "function") {
+	register() {
+
+		for (let i = 0; i < arguments.length; ++i) {
+			if (typeof arguments[i] === "function") {
 				this.hooks.push(arguments[i]);
-			} else {
+			} else if (arguments[i] instanceof Array) {
 				// unpack array
-				for(var j = 0; j < arguments[i].length; ++j) {
+				for (let j = 0; j < arguments[i].length; ++j) {
 					this.hooks.push(arguments[i][j]);
 				}
 			}
@@ -30,13 +35,12 @@ class Hook {
 
 	/**
 	 * Removes a function
-	 * @example this.content.deregister(function(){...});
+	 * @example this.content.deregister(() => {...});
 	 */
-	deregister(func){
-		let hook;
+	deregister(func) {
+
 		for (let i = 0; i < this.hooks.length; i++) {
-			hook = this.hooks[i];
-			if (hook === func) {
+			if (this.hooks[i] === func) {
 				this.hooks.splice(i, 1);
 				break;
 			}
@@ -45,38 +49,48 @@ class Hook {
 
 	/**
 	 * Triggers a hook to run all functions
-	 * @example this.content.trigger(args).then(function(){...});
+	 * @example this.content.trigger(args).then(() => {...});
+	 * @returns {Promise[]}
 	 */
-	trigger(){
-		var args = arguments;
-		var context = this.context;
-		var promises = [];
+	trigger() {
 
-		this.hooks.forEach(function(task) {
+		const args = arguments;
+		const context = this.context;
+		const promises = [];
+		let executing;
+
+		this.hooks.forEach((task) => {
 			try {
-				var executing = task.apply(context, args);
+				executing = task.apply(context, args);
 			} catch (err) {
-				console.log(err);
+				console.error(err);
 			}
 
-			if(executing && typeof executing["then"] === "function") {
+			if (executing && typeof executing["then"] === "function") {
 				// Task is a function that returns a promise
 				promises.push(executing);
 			}
-			// Otherwise Task resolves immediately, continue
 		});
-
 
 		return Promise.all(promises);
 	}
 
-	// Adds a function to be run before a hook completes
-	list(){
+	/**
+	 * list
+	 * @returns {Array}
+	 */
+	list() {
+
 		return this.hooks;
 	}
 
-	clear(){
-		return this.hooks = [];
+	/**
+	 * clear
+	 */
+	clear() {
+
+		this.hooks = [];
 	}
 }
+
 export default Hook;
