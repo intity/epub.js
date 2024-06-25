7cae1681ec663f379b9e0c0434f6f6df1a88f572
vite <nuget@list.ru>
2024-06-25 04:42:01 +0300

    updating cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -11,12 +11,14 @@ module.exports = defineConfig({
         },
         excludeSpecPattern: [
             // "cypress/e2e/book.cy.js",
-            // "cypress/e2e/core.cy.js",
             // "cypress/e2e/epub.cy.js",
             // "cypress/e2e/epubcfi.cy.js",
             // "cypress/e2e/locations.cy.js",
+            // "cypress/e2e/packaging.cy.js",
+            // "cypress/e2e/path.cy.js",
             // "cypress/e2e/section.cy.js",
-            // "cypress/e2e/themes.cy.js"
+            // "cypress/e2e/themes.cy.js",
+            // "cypress/e2e/url.cy.js"
         ]
     }
 })
